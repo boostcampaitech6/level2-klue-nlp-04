3b80f647b6a210038305fc12300cd6f9d26fceb2
kooqooo <koo9717@gmail.com>
2024-01-06 19:30:39 +0900

    fix: 포맷팅 적용
    #11

--- code/split_valid_stratify.py
+++ code/split_valid_stratify.py
@@ -6,10 +6,10 @@ Description:
    3. 호출 : split_stratify_valid(cfg["path"]["train_path"], cfg["path"]["valid_path"])
 """
 
-from sklearn.model_selection import train_test_split
 import pandas as pd
 import yaml
-from sklearn.model_selection import StratifiedKFold
+from modify_path import modify_path_to_upper_directory
+from sklearn.model_selection import train_test_split
 
 
 # yaml 파일 불러오기
@@ -18,19 +18,20 @@ def load_config(config_file):
         config = yaml.safe_load(file)
     return config
 
+
 # 2. 클래스 분포 비율 맞춰서 - 8:2로 train_new, valid.csv 파일 쪼개기
 def split_stratify_valid(train_path, valid_path):
     # train.csv 파일을 읽어오기
     df_train = pd.read_csv(train_path)  # 'train.csv'의 경로를 입력해야 합니다.
 
     # 각 클래스 별로 데이터를 나누기
-    unique_labels = df_train['label'].unique()
+    unique_labels = df_train["label"].unique()
 
     dfs_train = []
     dfs_valid = []
 
     for label in unique_labels:
-        df_label = df_train[df_train['label'] == label]
+        df_label = df_train[df_train["label"] == label]
 
         # label 클래스에 해당하는 데이터를 8:2 비율로 train과 valid로 분할
         df_train_label, df_valid_label = train_test_split(df_label, test_size=0.2, random_state=42)
@@ -44,15 +45,16 @@ def split_stratify_valid(train_path, valid_path):
 
     # 클래스 분포 확인
     print("Train 클래스 분포:")
-    print(df_train['label'].value_counts(normalize=True))
+    print(df_train["label"].value_counts(normalize=True))
     print("\nValid 클래스 분포:")
-    print(df_valid['label'].value_counts(normalize=True))
+    print(df_valid["label"].value_counts(normalize=True))
 
     # train.csv, valid.csv로 분할된 데이터를 저장
-    df_train.to_csv('/data/ephemeral/dataset/train/train_new.csv', index=False)
+    df_train.to_csv("/data/ephemeral/dataset/train/train_new.csv", index=False)
     df_valid.to_csv(valid_path, index=False)
 
-if __name__ == '__main__':
-    cfg = load_config("config.yaml") # yaml 파일 불러오기
+
+if __name__ == "__main__":
+    cfg = load_config("config.yaml")  # yaml 파일 불러오기
+    cfg = modify_path(cfg)
     split_stratify_valid(cfg["path"]["train_path"], cfg["path"]["valid_path"])
-   
