53c3d068ddec97548fb587b928053ec33b5c2825
maxseats <your.email@example.com>
2024-01-12 05:55:57 +0000

    feat: Query + Type Entity Marker
    * sentence의 앞에 질문 형태의 쿼리를 추가합니다.
    
    * 추가할 때, Type Entity Marker의 형식에 맞게 추가해줍니다.
    
    * add_query.py는 파일 경로를 config.yaml과 연동하지 않았습니다. 직접 설정해주세요.
    
    #48

---
+++ utils/add_query.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from preprocessing import Preprocess
+
+
+# train.csv 파일 불러오기
+file_path = 'train.csv'  # 파일 경로를 실제 파일 경로로 변경해야 합니다.
+df = pd.read_csv(file_path)
+
+
+# Preprocess 클래스 활용
+preprocessor = Preprocess(df)
+
+# 전처리 수행
+df_processed = preprocessor.add_qa_relation(df)
+
+# entity marker
+#df_entity = Preprocess(df_processed).entity_marker(df_processed)
+#df_entity.to_csv("new_new.csv")
+
+# typed entity marker
+df_typed_entity = preprocessor.typed_entity_marker(df_processed)
+df_typed_entity['sentence'] = df_processed['qa_relation'] + ' [SEP] ' + df_typed_entity['sentence'] + '[SEP]'
+df_typed_entity.to_csv("new.csv")
